2d8750da7dd57bf4340e3ba0cf9dd95a4ef795a3
Max Trader <141584547+MaxEphra@users.noreply.github.com>
2025-01-16 19:13:55 -0300

    Start: Glo Sust

---
+++ script.js
@@ -0,0 +1,11 @@
+ // Funções para abrir e fechar os modais
+ function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'flex';
+  }
+
+  function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+  }
+
+
+  
